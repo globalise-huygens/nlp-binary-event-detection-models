988b7239a6f098865b6bea2d1336903c4f0e08df
StellaVerkijk <stellaverkijk@hotmail.com>
2025-05-13 15:36:24 +0200

    update finetune.sh for general code running finetuning all models

--- finetune.sh
+++ finetune.sh
@@ -2,15 +2,14 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=18
-#SBATCH --gpus=2
-#SBATCH --partition=gpu
-#SBATCH --time=1:00:00
-#SBATCH --output=macberth.out
+#SBATCH --gpus=1
+#SBATCH --partition=gpu_a100
+#SBATCH --time=2:00:00
+#SBATCH --output=outfile_snellius.out
 
 module load 2022
 module load Python/3.10.4-GCCcore-11.3.0
 module load PyTorch/1.12.0-foss-2022a-CUDA-11.7.0
-
 #module load 2023
 #module load Python/3.11.3-GCCcore-12.3.0
 #module load PyTorch/2.1.2-foss-2023a-CUDA-12.1.1
@@ -26,7 +25,7 @@ scp -r $HOME/globalise/operation_autumn/data "$TMPDIR"/data
 
 # Define the array
 inv_nrs=('1160' '1066' '7673' '11012' '9001' '1348' '4071' '1090' '1430' '2665' '1439' '1595' '2693' '3476' '8596') #, '3598')
-modelnames=('emanjavacas/GysBERT-v2' 'FacebookAI/xlm-roberta-base' 'pdelobelle/robbert-v2-dutch-base' 'emanjavacas/GysBERT' 'GroNLP/bert-base-dutch-cased' 'bert-base-multilingual-cased', 'emanjavacas/MacBERTh', 'FacebookAI/roberta-base' 'google-bert/bert-base-cased')
+modelnames=('globalise/GloBERTise' 'bert-base-multilingual-cased' 'emanjavacas/GysBERT-v2' 'FacebookAI/xlm-roberta-base' 'pdelobelle/robbert-v2-dutch-base' 'emanjavacas/GysBERT' 'GroNLP/bert-base-dutch-cased' 'emanjavacas/MacBERTh' 'FacebookAI/roberta-base' 'google-bert/bert-base-cased')
 seeds=(23052024 21102024 553311 6834 888)
 
 
